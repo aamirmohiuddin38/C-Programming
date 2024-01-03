70f22fc1cb3aa050548f3e8fa4533964e3315d4c
aamirmohiuddin38 <eramirmohiuddin@gmail.com>
2024-01-03 21:36:16 +0530

    HelloWorld program

---
+++ HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+    system("cls");
+
+    printf("Hello world! Let's C\n");
+    printf("-----------------------\n\n");
+
+    return 0;
+}
