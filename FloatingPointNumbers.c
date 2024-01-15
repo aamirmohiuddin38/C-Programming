029289ec987288a8b27495afc5985afd7e90fce1
aamirmohiuddin38 <eramirmohiuddin@gmail.com>
2024-01-15 22:04:55 +0530

    int-float compatibility

--- FloatingPointNumbers.c
+++ FloatingPointNumbers.c
@@ -25,6 +25,22 @@ int main(void){
     printf("\t Type3 = %g\n", fnum);
     printf("\t Type4 = %4.2g\n", fnum);
 
+    // int - float compatibility
+    int a, b;
+    float fa, fb;
+    printf("\n\t Int-Float Compatibility:-\n");
+    printf("\t----------------------------------\n");
+
+    a = 2;
+    fa = a;
+    fb = fa * 2;
+    b = fb;
+    //"When float is assinged to int, it gets TRUNCATED"
+    printf("\t a = %d\n", a);    
+    printf("\t fa = %f\n", fa);    
+    printf("\t fb = %f\n", fb);
+    printf("\t b = %d\n", b);
+
     printf("\n\n");
     return 0;
 }
