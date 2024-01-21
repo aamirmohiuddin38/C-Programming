d38d15d762e6ab150ff366c8d47fad4b0fe0598f
aamirmohiuddin38 <eramirmohiuddin@gmail.com>
2024-01-21 18:13:52 +0530

    why use const?

--- CircleParameters.c
+++ CircleParameters.c
@@ -31,3 +31,13 @@ int main(void){
     printf("\n");
     return 0;
 }
+
+                //Declaring a constant: 'const'
+
+/*A variable is something that can change - that's why the name variable.
+So whereever we use it in the program, at the time of execution, each time the processor will go to the
+memory and get its latest value, because it can change anytime.[thats what it is supposed to do]
+But for example pi = 3.14, it is not going to change if it is used 10 times or 20 times in a program, so 
+we don't want processor go the memory and get the value ( as it is never going to change ), so when we declare 
+pi as 'const' , its value is embedded in the program where we have used pi, so it will become part of the program
+itself, processor doesn't need to go the memory and thus makes execution faster.*/
